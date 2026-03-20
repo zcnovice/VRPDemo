711bd5a4e2d4619f49eda2c9b460499a17e0120d
zcnovice <2739650633@qq.com>
2026-03-20 11:13:38 +0800

    项目初始化

---
+++ src/main/java/com/example/vrpdemo/VrpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vrpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrpDemoApplication.class, args);
+    }
+
+}
